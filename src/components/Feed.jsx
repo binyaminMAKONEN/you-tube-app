e84e65bcdf7b1fefed5361683922b9cbeca9aa13
benjamin <bnymynmqwnn@gmail.com>
2022-09-06 01:00:20 +0300

    sideBar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import { useState,useEffect } from 'react'
+import {Stack , Box ,Typography} from "@mui/material"
+import Sidebar from './Sidebar'
+import Videos from "./Videos"
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack  sx={{flexDirection:{sx:"column",md:"row"}
+    }}>
+        <Box sx={{height:{sx:"auto",md:"92vh"},borderRight: "1px solid #3d3d3d"
+        ,px:{sm:0,md:2}}}
+        >
+            <Sidebar/>
+            <Typography className='copyright'
+            variant='body2' sx={{mt:1.5,color:'#fff'}}>
+                Copyright benjamin media
+            </Typography>
+
+        </Box>
+        <Box p={2} sx={{overflowY:"auto",height:"90vh",flex:2}}>
+            <Typography variant='h4'
+            fontWeight="bold" mb={2} sx={{color:"white"}} >
+              New<span style={{color:"#F31503"}}>videos</span>
+            </Typography>
+            <Videos/>
+        </Box>
+    </Stack>
   )
 }
 
