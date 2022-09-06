be068cfc43de9789d16dead1a886789bc340436c
benjamin <bnymynmqwnn@gmail.com>
2022-09-06 21:39:05 +0300

    commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,14 +3,25 @@ import { useState,useEffect } from 'react'
 import {Stack , Box ,Typography} from "@mui/material"
 import Sidebar from './Sidebar'
 import Videos from "./Videos"
+import { fetchFromApi } from './utils/fetchFromApi'
 const Feed = () => {
+    const [selectedCategory,setSelectedCategory] =useState('New')
+    const [videos,setVideos] =useState([])
+    useEffect(() => {
+        fetchFromApi(`${selectedCategory}`)
+        .then((data)=>setVideos(data.items))
+        
+        
+    },[selectedCategory])
   return (
     <Stack  sx={{flexDirection:{sx:"column",md:"row"}
     }}>
         <Box sx={{height:{sx:"auto",md:"92vh"},borderRight: "1px solid #3d3d3d"
         ,px:{sm:0,md:2}}}
         >
-            <Sidebar/>
+            <Sidebar
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}/>
             <Typography className='copyright'
             variant='body2' sx={{mt:1.5,color:'#fff'}}>
                 Copyright benjamin media
@@ -20,9 +31,9 @@ const Feed = () => {
         <Box p={2} sx={{overflowY:"auto",height:"90vh",flex:2}}>
             <Typography variant='h4'
             fontWeight="bold" mb={2} sx={{color:"white"}} >
-              New<span style={{color:"#F31503"}}>videos</span>
+              {selectedCategory}<span style={{color:"#F31503"}}>videos</span>
             </Typography>
-            <Videos/>
+            <Videos videos={videos}/>
         </Box>
     </Stack>
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import { categories } from "./utils/constants";
-const selectedCategory = "New";
-const Sidebar = () => {
+const Sidebar = ({selectedCategory,setSelectedCategory}) => {
   return (
     <Stack
       direction="row"
@@ -15,6 +14,7 @@ const Sidebar = () => {
       {categories.map((category) => (
         <button
           className="category-btn"
+          onClick={()=>setSelectedCategory(category.name)}
           style={{
             background: category.name === selectedCategory && "#FC1503",color:"white"
         }}
